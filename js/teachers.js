88eebba6c64535e78284fd0b0653e6c46605be9c
cesavendano@wl.k12.ia.us <cesavendano@wl.k12.ia.us>
2018-11-19 09:27:18 -0600

    Fav Teachers

---
+++ js/teachers.js
@@ -0,0 +1,38 @@
+var teachers = [
+    {
+        image: "diemer.jpg",
+        name: "Diemer",
+        subjects: ["U.S. History"],
+        teacher_id: "1"
+    },
+    {
+        image: "lamb.jpg",
+        name: "Lamb",
+        subjects: ["Earth Science", "Astronomy", "Science"],
+        teacher_id: "2"
+    },
+    {
+        image: "czarnecki.jpg",
+        name: "Czarnecky",
+        subjects: ["P.E.", "Strength and Conditioning"],
+        teacher_id: "3"
+    },
+    {
+        image: "nelson.jpg",
+        name: "Nelson",
+        subjects: ["English"],
+        teacher_id: "4"
+    },
+    {
+        image: "jennings.jpg",
+        name: "Jennings",
+        subjects: ["English"],
+        teacher_id: "5"
+    },
+    {
+        image: "larson.jpg",
+        name: "Larson",
+        subjects: ["English"],
+        teacher_id: "6"
+    }
+];
